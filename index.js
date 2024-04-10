4871d35576e9b4a8b2186efb5ee6f221d992edb5
Prashant2303 <prashant1997.official@gmail.com>
2024-04-11 04:04:54 +0530

    Added Rotating words using CSS

--- index.js
+++ index.js
@@ -1,17 +1,4 @@
 (function () {
-    // Dynamic Text
-    const dynamicTextArray = ['Unnecessary', 'Inaccessible', 'Slow', 'kinda Cool'];
-    const dynamicText = document.querySelector('#dynamic-text');
-    console.log(dynamicText.innerHTML, localStorage.getItem('dark-mode'));
-
-    let i = 0;
-    setInterval(() => {
-        if (i === dynamicTextArray.length) {
-            i = 0;
-        }
-        dynamicText.innerHTML = dynamicTextArray[i++];
-    }, 2000)
-
     // Theme Toggle
     const bodyEl = document.querySelector('body');
     const lightMode = 'light-mode';
